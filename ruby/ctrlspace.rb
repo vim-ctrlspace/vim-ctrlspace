281f9d2af93f23ea779ae9c2182ea59010dd8242
Szymon Wrozynski <szymon@wrozynski.com>
2014-03-08 07:46:47 +0100

    Fix Ruby bits

--- ruby/ctrlspace.rb
+++ ruby/ctrlspace.rb
@@ -46,7 +46,7 @@ module CtrlSpace
           offset         = subseq[1][0] + 1
           matched_string = bufname[subseq[1][0]..subseq[1][-1]]
 
-          if !search_resonators.empty?
+          unless search_resonators.empty?
             noise += 1 if (subseq[1][0] == 0) || !search_resonators.include?(bufname[subseq[1][0] - 1])
             noise += 1 if (subseq[1][-1] == bufname_len - 1) || !search_resonators.include?(bufname[subseq[1][-1] + 1])
           end
